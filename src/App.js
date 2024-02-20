6eaf5aadb390af673a24e8ed3127f4cf1d5b3a84
Pranav Bedre <pranavbedre@gmail.com>
2024-02-20 15:28:19 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator, Button, Heading, Image, View, Card } from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className="App">
-      <h1>motorcycle finder app</h1>
-    </div>
+    <View className="App">
+      <Card>
+        {/* <Image src={logo} className="App-logo" alt="logo" /> */}
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
