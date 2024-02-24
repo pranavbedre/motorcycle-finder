433b1cb3f49556de151da70c92d83405dadae8d0
Pranav Bedre <pranavbedre@gmail.com>
2024-02-24 14:35:53 +0530

    AppBar with signout prototype

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "@aws-amplify/ui-react/styles.css";
-import { withAuthenticator, Button, Heading, Image, View, Card } from "@aws-amplify/ui-react";
+// import { withAuthenticator, Button, Heading, Image, View, Card } from "@aws-amplify/ui-react";
+import { withAuthenticator, Heading, Image, View, Card } from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
+import AppBarComp from "./components/AppBarComp";
 
-function App({ signOut }) {
+// function App({ signOut }) {
+function App() {
   return (
     <View className="App">
-      <Card>
-        {/* <Image src={logo} className="App-logo" alt="logo" /> */}
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
+      <AppBarComp />
     </View>
   );
 }
